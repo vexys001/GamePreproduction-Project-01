0c6e37b6a83d3859e2a47772e94d81918d1962db
YvesMor <yvesmorin001@gmail.com>
2022-09-16 18:43:08 -0400

    Claw Return

--- Assets/Scripts/Claw.cs
+++ Assets/Scripts/Claw.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class Claw : MonoBehaviour
 {
     public int acceleration = 5;
+    public int ReturnSpeed = 5;
     public bool DEBUG;
 
+    [Header("Item Holding")]
     public LayerMask GrabbingMask;
     public Transform HoldPos;
     public GameObject HeldGO;
     public bool Holding;
+
+    [Header("Dumping")]
+    public Transform DumpPos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,36 +29,55 @@ public class Claw : MonoBehaviour
         float hori = Input.GetAxisRaw("Horizontal");
         float vert = Input.GetAxisRaw("Vertical");
 
-        transform.Translate(new Vector3(hori, 0, vert) * acceleration * Time.deltaTime);
+        //transform.Translate(new Vector3(hori, 0, vert) * acceleration * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.E))
+        if (!Holding)
         {
-            if (!Holding)
+            transform.Translate(new Vector3(hori, 0, vert) * acceleration * Time.deltaTime);
+
+            if (Input.GetKeyDown(KeyCode.E))
             {
                 RaycastHit[] hits = Physics.RaycastAll(transform.position, Vector3.down, 25, GrabbingMask);
                 if (hits.Length > 0)
                 {
-                    HeldGO = hits[0].collider.gameObject;
-
-                    HeldGO.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-                    HeldGO.transform.SetParent(HoldPos, true);
-                    HeldGO.transform.localPosition = Vector3.zero;
-
-                    //HeldGO.transform.localScale = Vector3.one;
-
-                    Holding = true;
+                    TakeObject(hits[0].collider.gameObject);
                 }
+            }
+        }
+        else
+        {
+            Vector3 mvt = DumpPos.position - transform.position;
+
+            if (mvt.magnitude >= 0.05f)
+            {
+                transform.Translate(mvt.normalized * ReturnSpeed * Time.deltaTime);
             }
             else
             {
-                HeldGO.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-                HeldGO.transform.SetParent(null);
-
-                HeldGO = null;
-                Holding = false;
+                DropObject();
             }
-
         }
+
+    }
+
+    private void TakeObject(GameObject GameObj)
+    {
+        HeldGO = GameObj;
+
+        HeldGO.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        HeldGO.transform.SetParent(HoldPos, true);
+        HeldGO.transform.localPosition = Vector3.zero;
+
+        Holding = true;
+    }
+
+    private void DropObject()
+    {
+        HeldGO.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        HeldGO.transform.SetParent(null);
+
+        HeldGO = null;
+        Holding = false;
     }
 
     private void OnDrawGizmos()
