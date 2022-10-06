e67a766c9c7d57e08512840aa7197b4b335f0292
cboisvert <carlboisvert2012@gmail.com>
2022-10-06 19:24:20 -0400

    Pimp main screen

---
+++ Assets/Scripts/MenuNavigation.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuNavigation : MonoBehaviour
+{
+    [SerializeField] private GameObject _mainMenu;
+    [SerializeField] private GameObject _controlMenu;
+    [SerializeField] private Button _backButton;
+    [SerializeField] private Button _controlButton;
+
+
+    private void OnEnable()
+    {
+        _controlButton.onClick.AddListener(OnControlPressed);
+        _backButton.onClick.AddListener(OnBackPressed);
+    }
+
+    void OnControlPressed()
+    {
+        _mainMenu.SetActive(false);
+        _controlMenu.SetActive(true);
+    }
+    
+    void OnBackPressed()
+    {
+        _mainMenu.SetActive(true);
+        _controlMenu.SetActive(false);
+    }
+}
